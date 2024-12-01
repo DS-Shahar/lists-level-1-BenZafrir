859be981eb0bba75153ebc8afb0ae400ac7510e6
BenZafrir <zafrir.ben@gmail.com>
2024-12-01 22:50:22 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,25 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main{
+  public static List<Integer> generateRandomList(int x, int y) {
+        List<Integer> randomList = new ArrayList<>();
+        Random rand = new Random();
+        
+        for (int i = 0; i < 20; i++) {
+            int randomNumber = rand.nextInt(y - x + 1) + x;
+            randomList.add(randomNumber);
+        }
+        return randomList;
+  }
+  public static int countOccurrences(List<Integer> list, int x) {
+        int count = 0;
+        for (int number : list) {
+            if (number == x) {
+                count++;
+            }
+        }
+        return count;
   }
 }
